40741f0e2500eadb25c3c27ce4a6fcaeca1cbd56
NicoSwan <nicolaaswanepoel@gmail.com>
2025-08-17 01:56:47 +0200

    linting/checks

--- airflow/dags/compute_metrics.py
+++ airflow/dags/compute_metrics.py
@@ -39,7 +39,7 @@ def ensure_table():
       );
     """
     )
-    # helpful composite index for fast dedupe/queries
+    # helpful composite index for fast dedupe/query
     cur.execute(
         """
       CREATE INDEX IF NOT EXISTS ix_metrics_daily_day_city_var_h
