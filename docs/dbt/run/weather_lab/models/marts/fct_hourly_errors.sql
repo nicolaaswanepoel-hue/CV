b81cecb8549357d95e89d98b0fb0e2ee0a62d81b
NicoSwan <nicolaaswanepoel@gmail.com>
2025-08-18 20:57:59 +0200

    data refresh

--- docs/dbt/run/weather_lab/models/marts/fct_hourly_errors.sql
+++ docs/dbt/run/weather_lab/models/marts/fct_hourly_errors.sql
@@ -3,7 +3,7 @@
         delete from "airflow"."analytics_mart"."fct_hourly_errors" as DBT_INTERNAL_DEST
         where (city, valid_time) in (
             select distinct city, valid_time
-            from "fct_hourly_errors__dbt_tmp183202063584" as DBT_INTERNAL_SOURCE
+            from "fct_hourly_errors__dbt_tmp185614515687" as DBT_INTERNAL_SOURCE
         );
 
     
@@ -11,6 +11,6 @@
     insert into "airflow"."analytics_mart"."fct_hourly_errors" ("city", "valid_time", "temp_c", "precip_mm", "model_run", "temp_c_forecast", "precip_mm_forecast", "abs_temp_err", "abs_precip_err")
     (
         select "city", "valid_time", "temp_c", "precip_mm", "model_run", "temp_c_forecast", "precip_mm_forecast", "abs_temp_err", "abs_precip_err"
-        from "fct_hourly_errors__dbt_tmp183202063584"
+        from "fct_hourly_errors__dbt_tmp185614515687"
     )
   
